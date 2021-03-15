ed81eb3af9ff866e7a99f762f70e5560ae9da15f
Jess247 <jesss8692@hotmail.com>
2021-03-15 11:18:30 +0100

    add semicolon

---
+++ progress/js/app.js
@@ -0,0 +1,22 @@
+const btnTapped = document.querySelector('#btn').addEventListener('click', addProgress);
+const progressBar = document.querySelector('#progress-bar');
+const progressLabel = document.querySelector('#progress-label');
+const intervalProgress = document.querySelector('#animated-progress');
+var count = 0;
+
+// progress on btn press
+function addProgress() {
+count++;
+progressBar.style.width = count + "%";
+progressLabel.textContent = count + "%";
+}
+
+// automatic progress
+var  progressCounter = 0;
+
+setInterval(function(){
+    progressCounter++;
+    // console.log(progressCounter);
+    intervalProgress.style.width = progressCounter + "%";
+}, 600);
+
