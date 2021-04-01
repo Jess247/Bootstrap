15cdbe5a28cbbbe17cb4d2df261245def538715f
Jess247 <jesss8692@hotmail.com>
2021-04-01 16:12:46 +0200

    add js
    on btn press spinner shows. so far its not disappearing

--- spinner/js/app.js
+++ spinner/js/app.js
@@ -1,1 +1,8 @@
-const uploadBtn = document.querySelector('#show-spinner');
+let uploadBtn = document.querySelector('#upload-btn');
+uploadBtn.addEventListener('click', function() {
+        document.querySelector('#show-spinner').classList.add('spinner-border');
+        uploadBtn.classList.add('disabled');
+});
+
+
+
