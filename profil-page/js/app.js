f669df27c1152afad8dcb11ca4a1e2f5277f10e7
Jess247 <jesss8692@hotmail.com>
2021-05-10 18:16:33 +0200

    update file upload

---
+++ profil-page/js/app.js
@@ -0,0 +1,30 @@
+const changeImgBtn = document.querySelector("#profil-edit-btn");
+const closeBtn = document.querySelector("#close-btn")
+const overlayForm = document.querySelector("#overlay");
+
+changeImgBtn.addEventListener("click", displayForm);
+
+
+function displayForm() {
+    overlayForm.style.display = "block";
+}
+
+// upload
+const uploadeForm = document.querySelector("#uploadeForm");
+const inpFile = document.querySelector("inpFile");
+
+uploadeForm.addEventListener("submit", e => {
+    // prevent page form reloading
+    e.preventDefault();
+
+    const endpoint = "upload.php";
+    const formData = new FormData();
+
+    formData.append("inpFile", inpFile.files[0]);
+
+    fetch(endpoint, {
+        method: "post",
+        body: formData
+    }.catch(console.error));
+});
+
